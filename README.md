6890eb06749fed5111af1736cc40002855e1533d
Lewis Monteith <lewismonteith@gmail.com>
2021-03-18 15:38:24 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Serverless HTTP Utils
+
+A small utils package that simplifies REST handlers inside Serverless TypeScript APIs.
+
+### Requirements
+- A Serverless API using Typescript
+- ApiGateway configured to use the v2 event
+
+### Installation
+```shell
+$ yarn add @lemonjs/serverless-http-utils
+or
+$ npm install @lemonjs/serverless-http-utils
+```
+
+### Usage
+Wrap any exported Serverless handler with the `handler` method and return a new instance of `HttpSuccess`:
+```typescript
+import { handler, Event, HttpSuccess } from '@lemonjs/serverless-http-utils';
+
+export const handle = handler((event: Event) => {
+  return new HttpSuccess(200, { hello: 'world' });
+});
+```
+Errors can be thrown anywhere by throwing a `HttpException`:
+```typescript
+import { HttpException } from '@lemonjs/serverless-http-utils';
+
+async function authenticate(token: string): void {
+  if (!token) {
+    throw new HttpException(401, 'Token not provided');
+  }
+}
+```
+See the `examples/` folder for more info.
+
+### Contributing
+Clone the repository:
+```shell
+$ git clone git@github.com:lemonJS/serverless-http-utils.git
+```
+Install dependencies:
+```shell
+$ yarn install
+```
+Run the tests:
+```shell
+$ yarn test
+```
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyEventHeaders, APIGatewayProxyEventV2 } from 'aws-lambda';
+
+export type Event = APIGatewayProxyEventV2;
+
+export type HandlerFunction = (event: Event) => Promise<HttpSuccess>;
+
+export class HttpSuccess {
+  public statusCode: number;
+  public body: string;
+  public headers: APIGatewayProxyEventHeaders;
+
+  public constructor(statusCode: number, body: any, headers?: APIGatewayProxyEventHeaders) {
+    this.statusCode = statusCode;
+    this.body = JSON.stringify(body);
+    this.headers = headers || {};
+  }
+}
+
+export class HttpException extends Error {
+  public statusCode: number;
+  public body: string;
+
+  public constructor(statusCode: number, message: string) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.body = JSON.stringify({ error: message });
+  }
+}
+
+/**
+ * Custom http handler that returns a structured
+ * response.
+ * @param {HandlerFunction} handle
+ * @return {Promise<HttpSuccess | HttpException>}
+ */
+export const handler = (handle: HandlerFunction) => async (event: Event) => {
+  return handle(event).catch(error => {
+    return error instanceof HttpException
+      ? error
+      : new HttpException(500, 'Internal Server Error');
+  });
+};
